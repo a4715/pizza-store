04ae9df6fa0252674c4c44e5487d972257d0cc45
adhav <adhavanbalagan@gmail.com>
2025-10-24 10:45:50 +0800

    Pizza Store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div>
+      <Header />
+      <PizzaImage />
+      <p><b>Margherita Pizza</b></p>
+      <p>Tomato, Mozzarella Cheese</p>
+      <p>10</p>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+  return (
+    <div>
+      <h2>Adhav's Pizza Co.</h2>
+    </div>
+  );
+}
+
+function PizzaImage() {
+  return (
+    <div>
+    <img src="margherita.jpg" alt="Margherita Pizza"></img>  
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
